666db3ea397b6772a2fc4ca54d7f132e74792b3b
Diego de Souza Furlanetto <diegosouzafurlanetto@gmail.com>
2025-05-26 11:14:20 -0300

    Maui

--- AppTempoAgoraSQLite/MainPage.xaml.cs
+++ AppTempoAgoraSQLite/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace AppTempoAgoraSQLite
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void ButtonPrevisao_Clicked(object sender, EventArgs e)
         {
             try
             {
@@ -28,30 +28,49 @@ namespace AppTempoAgoraSQLite
                                          $"Longitude: {t.lon} \n" +
                                          $"Nascer do Sol: {t.sunrise} \n" +
                                          $"Por do Sol: {t.sunset} \n" +
-                                         $"Temp Máx: {t.temp_max} \n" +
-                                         $"Temp Min: {t.temp_min} \n";
+                                         $"Temperatura Máxima: {t.temp_max} \n" +
+                                         $"Temperatura Mínima: {t.temp_min} \n";
 
                         lbl_res.Text = dados_previsao;
 
-                        await App.Db.Insert(t);
-
+                        try
+                        {
+                            await App.Db.Insert(t);
+                        }
+                        catch (Exception ex)
+                        {
+                            await DisplayAlert("Você é um imundo!", ex.Message, "OK");
+                        }
                     }
                     else
                     {
-                        lbl_res.Text = "Sem dados de Previsão";
-                    } // Fecha if t=null
-
+                        lbl_res.Text = "Sem dados de previsão.";
+                    }
                 }
                 else
                 {
                     lbl_res.Text = "Preencha a cidade.";
-                } // fecha if string is null or empty
-
+                }
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Ops", ex.Message, "OK");
             }
+        }
+
+        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void lst_historico_Refreshing(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lst_historico_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 
